223ab186c8e21331198ea95ab9ee6b4a41fbfb38
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-08 18:35:03 +0900

    create 16.py

---
+++ effective_python/betteway16.py
@@ -0,0 +1,8 @@
+def index_words(text):
+    result = []
+    if text:
+        result.append(0)
+    for index, letter in enumerate(text):
+        if letter = ' ':
+            result.append(index + 1)
+    return result
