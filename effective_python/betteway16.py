fbe22bbba13f38ab92c20618d718eb2ba51037f6
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-08 18:55:49 +0900

    1

--- effective_python/betteway16.py
+++ effective_python/betteway16.py
@@ -1,3 +1,4 @@
+################                 BetterWay 16                 ################
 def index_words(text):
     result = []
     if text:
@@ -6,3 +7,4 @@ def index_words(text):
         if letter = ' ':
             result.append(index + 1)
     return result
+아룡
