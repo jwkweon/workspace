bc079182aa8b2dad422bc2ecc13d58905d1cccbd
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-10 00:11:00 +0900

    .

--- effective_python/betterway19.py
+++ effective_python/betterway19.py
@@ -13,3 +13,29 @@ a4 = remainder(divisor=7, number=20)
 #위치 인수는 키워드 인수 앞에 지정해야함
 
 print(a1, a2, a3, a4)
+
+def flow_rate(weight_diff, time_diff):
+    return weight_diff / time_diff
+
+weight_diff = 0.5
+time_diff = 3
+flow = flow_rate(weight_diff, time_diff)
+print('%.3f kg per second' % flow)
+
+def flow_rate(weight_diff, time_diff, period=1):
+    return (weight_diff / time_diff) * period
+
+flow_per_second = flow_rate(weight_diff, time_diff, 1)
+print('%.3f kg per second' % flow_per_second)
+
+flow_per_second = flow_rate(weight_diff, time_diff)
+print('%.3f kg per second' % flow_per_second)
+
+flow_per_hour = flow_rate(weight_diff, time_diff, 3600)
+print('%.3f kg per hour' % flow_per_hour)
+
+def flow_rate(weight_diff, time_diff, period=1, units_per_kg=1):
+    return ((weight_diff / units_per_kg) / time_diff) * period
+
+flow_per_hour = flow_rate(weight_diff, time_diff, period=3600, units_per_kg=2.2)
+print('%.3f kg per hour' % flow_per_hour)
