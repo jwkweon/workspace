7288578ec0817f7c1840c86062c5870b80dab4a4
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-09 23:30:03 +0900

    .

--- effective_python/betterway18.py
+++ effective_python/betterway18.py
@@ -1,2 +1,41 @@
 ################                 BetterWay 18                 ################
 # 가변 위치 인수로 깔끔하게 보이게 하자
+def log(message, values):
+    if not values:
+        print(message)
+    else:
+        value_str = ', '.join(str(x) for x in values)
+        print('%s: %s' % (message, value_str))
+
+log('My nums are', [1, 2])
+log('Hi there', [])
+
+def log(message, *values):
+    if not values:
+        print(message)
+    else:
+        value_str = ', '.join(str(x) for x in values)
+        print('%s: %s' % (message, value_str))
+
+log('My nums are', [1, 2])
+log('Hi there')
+
+favorites = [7, 33, 99]
+log('Favorite colors', *favorites)
+
+def my_generator():
+    for i in range(10):
+        yield i
+
+def my_func(*args):
+    print(args)
+
+it = my_generator()
+my_func(*it)
+
+def log(sequence, message, *values):
+    if not values:
+        print('%s: %s' % (sequence, message))
+    else:
+        value_str = ', '.join(str(x) for x in values)
+        print('%s: %s: %s' % (sequence, message, value_str))
