5dd208d98293da43a4a9df1aa8bd53bff3012b11
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-10 15:42:14 +0900

    .

--- effective_python/betterway21.py
+++ effective_python/betterway21.py
@@ -71,3 +71,27 @@ def print_args(*args, **kwargs):
     print('Keyword    :', kwargs)
 
 print_args(1, 2, foo='bar', stuff='meep')
+
+def safe_division_d(number, divisor, **kwargs):
+    ignore_overflow = kwargs.pop('ignore_overflow', False)
+    ignore_zero_division = kwargs.pop('ignore_zero_division', False)
+    if kwargs:
+        raise TypeError('Unexpected **kwargs: %r' % kwargs)
+    try:
+        return number / divisor
+    except OverflowError:
+        if ignore_overflow:
+            return 0
+        else:
+            raise
+    except ZeroDivisionError:
+        if ignore_zero_division:
+            return float('inf')
+        else:
+            raise
+
+safe_division_d(1, 0)
+safe_division_d(1, 0, ignore_overflow=True)
+safe_division_d(1, 0, ignore_zero_division=True)
+safe_division_d(1, 10**500, True, False)
+safe_division_d(1, 0, ignore_zero_divisi=True)
