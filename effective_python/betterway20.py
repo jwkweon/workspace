55e5d22c1248e2c5381c7c80c9c10ab14d079761
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-10 14:15:52 +0900

    .

---
+++ effective_python/betterway20.py
@@ -0,0 +1,64 @@
+################                 BetterWay 18                 ################
+# 동적 기본 인수를 지정하려면 None과 docstring을 사용하자
+from datetime import datetime
+import time
+import json
+
+def log(message, when=datetime.now()):
+    print('%s: %s' % (when, message))
+
+log('Hi there')
+time.sleep(0.1)
+log('Hi again!')
+
+def log(message, when=None):
+    """Log a message with a timestamp.
+
+    Args:
+        message: Message to print.
+        when: datetiem of when the message occurred.
+            Defaults to the present time.
+    """
+    when = datetime.now() if when is None else when
+    print('%s: %s' % (when, message))
+
+log('Hi there')
+time.sleep(0.1)
+log('Hi again!')
+
+def decode(data, default={}):
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
+
+foo = decode('bad data')
+foo['stuff'] = 5
+bar = decode('also bad')
+bar['meep'] = 1
+print('Foo:', foo)
+print('bar:', bar)
+
+assert foo is bar
+
+def decode(data, default=None):
+    """Load JSON data from a string.
+
+    Args:
+        data: JSON data to decode.
+        default: Value to return if decoding fails.
+            Defaults to an empty dictionary.
+    """
+    if default is None:
+        default = {}
+    try:
+        return json.loads(data)
+    except ValueError:
+        return default
+
+foo = decode('bad data')
+foo['stuff'] = 5
+bar = decode('also bad')
+bar['meep'] = 1
+print('Foo:', foo)
+print('bar:', bar)
