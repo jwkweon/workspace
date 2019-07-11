e6b4148a747214514072250f5296b263fe516f99
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-11 13:58:55 +0900

    .

--- effective_python/betterway22.py
+++ effective_python/betterway22.py
@@ -19,3 +19,33 @@ book.add_student('Isaac Newton')
 book.report_grade('Isaac Newton', 90)
 
 print(book.average_grade('Isaac Newton'))
+print(book._grades)
+
+class BySubjectGradebook(object):
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = {}
+
+    def report_grade(self, name, subject, grade):
+        by_subject = self._grades[name]
+        grade_list = by_subject.setdefault(subject, [])
+        grade_list.append(grade)
+
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+        total, count = 0, 0
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+        return total / count
+
+book = BySubjectGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 75)
+book.report_grade('Albert Einstein', 'Math', 65)
+book.report_grade('Albert Einstein', 'Gym', 90)
+book.report_grade('Albert Einstein', 'Gym', 95)
+book.average_grade('Albert Einstein')
+print(book._grades)
