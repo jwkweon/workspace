94865025225ed882b196639e2b42ca90061ae20f
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-11 14:32:09 +0900

    .

--- effective_python/betterway22.py
+++ effective_python/betterway22.py
@@ -49,3 +49,37 @@ book.report_grade('Albert Einstein', 'Gym', 90)
 book.report_grade('Albert Einstein', 'Gym', 95)
 book.average_grade('Albert Einstein')
 print(book._grades)
+
+class WeightedGradebook(object):
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = {}
+
+    def report_grade(self, name, subject, score, weight):
+        by_subject = self._grades[name]
+        grade_list = by_subject.setdefault(subject, [])
+        grade_list.append((score, weight))
+
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+        score_sum, score_count = 0, 0
+        for subject, scores in by_subject.items():
+            subject_avg, total_weight = 0, 0
+            for score, weight in scores:
+                subject_avg += score * weight
+                total_weight += weight
+            subject_avg /= total_weight
+            score_sum += subject_avg
+            score_count += 1
+        return score_sum / score_count
+
+book = WeightedGradebook()
+book.add_student('Albert Einstein')
+book.report_grade('Albert Einstein', 'Math', 80, 0.5)
+book.report_grade('Albert Einstein', 'Math', 90, 0.5)
+book.report_grade('Albert Einstein', 'Gym', 90, 1.0)
+book.report_grade('Albert Einstein', 'Gym', 80, 0.0)
+book.average_grade('Albert Einstein')
+print(book._grades)
