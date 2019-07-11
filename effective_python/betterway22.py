0f11fd9cc6bff4771de450f54997ee51a1fc181a
Juwon <34915209+zzu0203@users.noreply.github.com>
2019-07-11 16:04:32 +0900

    .

--- effective_python/betterway22.py
+++ effective_python/betterway22.py
@@ -86,4 +86,43 @@ print(book._grades)
 
 import collections
 
-Grade
+Grade = collections.namedtuple('Grade', ('score', 'weight'))
+
+class Subject(object):
+    def __init__(self):
+        self._grades = []
+
+    def report_grade(self, score, weight):
+        self._grades.append(Grade(score, weight))
+
+    def average_grade(self):
+        total, total_weight = 0, 0
+        for grade in self._grades:
+            total += grade.score * grade.weight
+            total_weight += grade.weight
+        return total / total_weight
+
+class Student(object):
+    def __init__(self):
+        self._subject = {}
+
+    def subject(self, name):
+        if name not in self._subject:
+            self._subject[name] = Subject()
+        return self._subject[name]
+
+    def average_grade(self):
+        total, count = 0, 0
+        for subject in self._subject.values():
+            total += subject.average_grade()
+            count += 1
+        return total / count
+
+class GradeBook(object):
+    def __init__(self):
+        self._students = {}
+
+    def student(self, name):
+        if name not in self._students:
+            self._students[name] = Student()
+        return self._students[name]
